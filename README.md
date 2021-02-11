12625317566ce5f27b4b676b85781815564f8b83
gabminamedez <gabminamedez@gmail.com>
2021-02-11 21:58:15 +0800

    Finalized web app and added details in README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # weekday
 I hate weekdays. Kanban board mock-up built with ReactJS.
+
+## Run this on your local machine 👨‍💻
+1. Download or clone this project.
+2. In the command line, go to the project directory.
+3. To install dependies, enter `npm install`.
+4. To run the application, enter `npm run start`.
+5. Open a browser and go to [http://localhost:3000/](http://localhost:3000/).
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container, Row } from 'reactstrap';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -6,65 +6,153 @@ import Card from './components/Card';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends Component {
-  state = {
-    tasks: [
-      { id: '001', title: 'Have progress w/ Weekday', content: 'Work on Drag and Drop context.' },
-      { id: '002', title: 'Study for STADVDB Finals', content: 'Make sure to focus on the transactions and OLAP queries.' },
-      { id: '003', title: 'Write In the Mood', content: 'Finish Chapters 2 and 3.' },
-      { id: '004', title: 'Trim fingernails', content: null },
-      { id: '005', title: 'Haircut', content: null },
-      { id: '006', title: 'Cook lunch', content: 'Chicken tenders, Lamb curry, Shrimp fried rice.' },
-      { id: '007', title: 'Read Kitchen Confidential', content: 'Finish 5 chapters.' },
-      { id: '008', title: 'Have progress w/ beat', content: 'Try to add the kicks and snares to the melody.' }
-    ],
-    cards: [
-      { id: '001', title: 'Have progress w/ Weekday', content: 'Work on Drag and Drop context.' },
-      { id: '002', title: 'Study for STADVDB Finals', content: 'Make sure to focus on the transactions and OLAP queries.' },
-      { id: '003', title: 'Write In the Mood', content: 'Finish Chapters 2 and 3.' },
-      { id: '004', title: 'Trim fingernails', content: null },
-      { id: '005', title: 'Haircut', content: null },
-      { id: '006', title: 'Cook lunch', content: 'Chicken tenders, Lamb curry, Shrimp fried rice.' },
-      { id: '007', title: 'Read Kitchen Confidential', content: 'Finish 5 chapters.' },
-      { id: '008', title: 'Have progress w/ beat', content: 'Try to add the kicks and snares to the melody.' }
-    ],
-    updateCards: [
-      { id: '001', title: 'Have progress w/ Weekday', content: 'Work on Drag and Drop context.' },
-      { id: '002', title: 'Study for STADVDB Finals', content: 'Make sure to focus on the transactions and OLAP queries.' },
-      { id: '003', title: 'Write In the Mood', content: 'Finish Chapters 2 and 3.' },
-      { id: '004', title: 'Trim fingernails', content: null },
-      { id: '005', title: 'Haircut', content: null },
-      { id: '006', title: 'Cook lunch', content: 'Chicken tenders, Lamb curry, Shrimp fried rice.' },
-      { id: '007', title: 'Read Kitchen Confidential', content: 'Finish 5 chapters.' },
-      { id: '008', title: 'Have progress w/ beat', content: 'Try to add the kicks and snares to the melody.' }
-    ]
-  }
+const arr1 = [
+  { id: '001', title: 'Have progress w/ Weekday', content: 'Work on Drag and Drop context.' },
+  { id: '002', title: 'Study for STADVDB Finals', content: 'Make sure to focus on the transactions and OLAP queries.' },
+  { id: '003', title: 'Write In the Mood', content: 'Finish Chapters 2 and 3.' },
+  { id: '004', title: 'Trim fingernails', content: null },
+  { id: '005', title: 'Haircut', content: null },
+  { id: '006', title: 'Cook lunch', content: 'Chicken tenders, Lamb curry, Shrimp fried rice.' },
+  { id: '007', title: 'Read Kitchen Confidential', content: 'Finish 5 chapters.' },
+  { id: '008', title: 'Have progress w/ beat', content: 'Try to add the kicks and snares to the melody.' }
+]
 
-  handleOnDragEnd = (result) => {
+const arr2 = []
+
+const arr3 = []
+
+function App() {
+  const [cards1, updateCards1] = useState(arr1);
+  const [cards2, updateCards2] = useState(arr2);
+  const [cards3, updateCards3] = useState(arr3);
+
+  function handleOnDragEnd(result){
     if (!result.destination) return;
 
-    const items = Array.from(this.state.cards);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if(result.source.droppableId === 'cards1' & result.destination.droppableId === 'cards1'){
+      const items = Array.from(cards1);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      updateCards1(items);
+    }
+    else if(result.source.droppableId === 'cards2' & result.destination.droppableId === 'cards2'){
+      const items = Array.from(cards2);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      updateCards2(items);
+    }
+    else if(result.source.droppableId === 'cards3' & result.destination.droppableId === 'cards3'){
+      const items = Array.from(cards3);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      updateCards3(items);
+    }
+    else if(result.source.droppableId === 'cards1' & result.destination.droppableId === 'cards2'){
+      const items_source = Array.from(cards1);
+      const items_dest = Array.from(cards2);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards1(items_source);
+      updateCards2(items_dest);
+    }
+    else if(result.source.droppableId === 'cards1' & result.destination.droppableId === 'cards3'){
+      const items_source = Array.from(cards1);
+      const items_dest = Array.from(cards3);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards1(items_source);
+      updateCards3(items_dest);
+    }
+    else if(result.source.droppableId === 'cards2' & result.destination.droppableId === 'cards1'){
+      const items_source = Array.from(cards2);
+      const items_dest = Array.from(cards1);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards2(items_source);
+      updateCards1(items_dest);
+    }
+    else if(result.source.droppableId === 'cards2' & result.destination.droppableId === 'cards3'){
+      const items_source = Array.from(cards2);
+      const items_dest = Array.from(cards3);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards2(items_source);
+      updateCards3(items_dest);
+    }
+    else if(result.source.droppableId === 'cards3' & result.destination.droppableId === 'cards1'){
+      const items_source = Array.from(cards3);
+      const items_dest = Array.from(cards1);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards3(items_source);
+      updateCards1(items_dest);
+    }
+    else if(result.source.droppableId === 'cards3' & result.destination.droppableId === 'cards2'){
+      const items_source = Array.from(cards3);
+      const items_dest = Array.from(cards2);
+      const [reorderedItem] = items_source.splice(result.source.index, 1);
+      items_dest.splice(result.destination.index, 0, reorderedItem);
+      updateCards3(items_source);
+      updateCards2(items_dest);
+    }
+  };
 
-    this.updateCards(items);
-  }
+  return(
+    <div className="App">
+      <Container>
+        <h1 className="name">Weekday.</h1>
+        <hr/>
 
-  render(){
-    return(
-      <div className="App">
-        <Container>
-          <h1 className="name">Weekday.</h1>
-          <hr/>
-
-          <Row xs="3">
-          <DragDropContext onDragEnd={this.handleOnDragEnd}>
-            <h3 className="heading">Not Started</h3>
-            <hr/>
-            <Droppable droppableId="cards">
+        <Row xs="3">
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="cards1">
               {(provided) => (
-                <ul className="list cards" id="notStarted" {...provided.droppableProps} ref={provided.innerRef}>
-                  {this.state.cards.map(({id, title, content}, index) => {
+                <ul className="list" id="notStarted" {...provided.droppableProps} ref={provided.innerRef}>
+                  <h3 className="heading">Not Started 🤷‍♂️</h3>
+                  <hr/>
+                  {cards1.map(({id, title, content}, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                        {(provided) => (
+                          <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Card title={title} content={content}></Card>
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+
+            <Droppable droppableId="cards2">
+              {(provided) => (
+                <ul className="list" id="inProgress" {...provided.droppableProps} ref={provided.innerRef}>
+                  <h3 className="heading">In Progress 🔜</h3>
+                  <hr/>
+                  {cards2.map(({id, title, content}, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                        {(provided) => (
+                          <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <Card title={title} content={content}></Card>
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+
+            <Droppable droppableId="cards3">
+              {(provided) => (
+                <ul className="list" id="completed" {...provided.droppableProps} ref={provided.innerRef}>
+                  <h3 className="heading">Completed ✅</h3>
+                  <hr/>
+                  {cards3.map(({id, title, content}, index) => {
                     return (
                       <Draggable key={id} draggableId={id} index={index}>
                         {(provided) => (
@@ -80,39 +168,10 @@ class App extends Component {
               )}
             </Droppable>
           </DragDropContext>
-
-            <div className="list" id="notStarted">
-              <h3 className="heading">Not Started</h3>
-              <hr/>
-
-              <ul>
-                <li><Card></Card></li>
-                <li><Card></Card></li>
-              </ul>
-            </div>
-
-            <div className="list" id="inProgress">
-              <h3 className="heading">In Progress</h3>
-              <hr/>
-
-              <ul>
-                <Card></Card>
-              </ul>
-            </div>
-
-            <div className="list" id="completed">
-              <h3 className="heading">Completed</h3>
-              <hr/>
-
-              <ul>
-                <Card></Card>
-              </ul>
-            </div>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
